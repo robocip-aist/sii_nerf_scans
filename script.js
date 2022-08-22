d6ca483cf374a398a04913fa4d4453f4fa63ec8b
Baptiste-AIST <bourreau.baptiste@aist.go.jp>
2022-08-22 15:37:46 +0900

    updated scan status

---
+++ script.js
@@ -0,0 +1,30 @@
+// Get the .gif images from the "data-alt"
+var getGif = function() {
+    var gif = [];
+
+    $('img').each(function() {
+      var data = $(this).data('alt');
+      gif.push(data);
+    });
+
+    return gif;
+}
+
+var gif = getGif();
+
+// Change the image to .gif when clicked and vice versa
+$('figure').on('click', function() {
+
+    var $this = $(this),            
+        $img    = $this.children('img'),
+        $imgSrc = $img.attr('src'),
+        $imgAlt = $img.attr('data-alt'),
+        $imgExt = $imgAlt.split('.');
+                
+    if($imgExt[1] === 'gif') {
+        $img.attr('src', $img.data('alt')).attr('data-alt', $imgSrc);
+    } else {
+        $img.attr('src', $imgAlt).attr('data-alt', $img.data('alt'));
+    }
+
+});
